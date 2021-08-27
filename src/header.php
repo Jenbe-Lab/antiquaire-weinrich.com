f7d9305e06669b2b967d36fbe0164364f0c49a0f
Mathieu Bour <mathieu.tin.bour@gmail.com>
2021-08-27 02:35:13 +0200

    fix: use Google's recommanded meta viewport

--- src/header.php
+++ src/header.php
@@ -2,8 +2,7 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo('charset'); ?>"/>
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php wp_head(); ?>
   </head>
@@ -25,6 +24,6 @@
         'menu'            => 'header',
         'container'       => 'nav',
         'container_class' => 'header__nav',
-        'before'          => '<div class="header__nav-toggle"></div>'
+        'before'          => '<div class="header__nav-toggle"></div>',
       ]); ?>
     </header>
