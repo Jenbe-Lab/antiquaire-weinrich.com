a15720a53a584eb537715c673c62d13cd3cbc69e
Mathieu Bour <mathieu@csquare.ai>
2021-08-28 19:52:43 +0200

    fix: make menu works on mobile (enters on left)

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,8 +1,29 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  const nav = document.getElementsByClassName('header__nav')[0];
-  const navToggle = document.getElementsByClassName('header__nav-toggle')[0];
+/**
+ * @param {HTMLElement} $nav
+ * @param {HTMLElement} $element
+ */
+const setupNav = ($nav, $element) => {
+  $element.addEventListener('click', () => $nav.classList.toggle('open'));
+};
 
-  navToggle.addEventListener('click', () => {
-    nav.classList.toggle('open');
-  });
+document.addEventListener('DOMContentLoaded', async () => {
+  let $nav, $toggle;
+
+  // Ugly die-and-retry loop but it works fine
+  for (let i = 0; i < 100; i++) {
+    $nav = document.getElementsByClassName('header__nav')[0];
+    $toggle = document.getElementsByClassName('header__nav-toggle')[0];
+
+    if (!$nav || !$toggle) {
+      await new Promise((resolve) => {
+        const t = setTimeout(() => {
+          clearTimeout(t);
+          resolve();
+        }, 100);
+      });
+    } else {
+      setupNav($nav, $toggle);
+      break;
+    }
+  }
 });
